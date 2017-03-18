4347afc4656f91319ff0c4c0a44cd30729def49b
cyper <345343747@qq.com>
2017-03-18 19:16:15 +0800

    virtual srolling and custom list header

--- src/pages/park-list/park-list.ts
+++ src/pages/park-list/park-list.ts
@@ -51,4 +51,16 @@ export class ParkListPage {
     })
   }
 
+  customHeaderFn(record, recordIndex, records) {
+    if (recordIndex > 0) {
+      if (record.name.charAt(0) !== records[recordIndex - 1].name.charAt(0)) {
+        return record.name.charAt(0);
+      } else {
+        return null;
+      }
+
+    } else {
+      return record.name.charAt(0);
+    }
+  }
 }
